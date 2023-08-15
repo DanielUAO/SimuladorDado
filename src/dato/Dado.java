25a656f0ca98b5028e56c993d14f64d037b8475a
SebastianEsatupinan <124362855+SebastianEsatupinan@users.noreply.github.com>
2023-08-15 07:45:52 -0500

    pueba de push

--- src/dato/Dado.java
+++ src/dato/Dado.java
@@ -6,6 +6,11 @@ public class Dado {
     
   private int dado1;
   private int dado2;
+
+    public Dado(int dado1, int dado2) {
+        this.dado1 = dado1;
+        this.dado2 = dado2;
+    }
   
     
 }
