445f3dad211dfdc1c008eb6a068ef128471d56f6
DanielUAO <daniel_alberto.paz@uao.edu.co>
2023-08-15 07:47:44 -0500

    Constructor vacio

--- src/dato/Dado.java
+++ src/dato/Dado.java
@@ -7,6 +7,10 @@ public class Dado {
   private int dado1;
   private int dado2;
 
+    public Dado() {
+    }
+  
+
     public Dado(int dado1, int dado2) {
         this.dado1 = dado1;
         this.dado2 = dado2;
