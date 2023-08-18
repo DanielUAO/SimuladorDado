9d499ac38ce67f45db4bbfd7aff6619f9455cfae
SebastianEsatupinan <sebastian.esatupinan@uao.edu.co>
2023-08-18 16:19:13 -0500

    Eliminación de los comentarios
    Saben que odio los comentarios que no son míos.

--- src/modelo/Pila.java
+++ src/modelo/Pila.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 import java.util.LinkedList;
 
-/**
- *
- * @author daniel_alberto.paz
- */
 public class Pila <T extends Base> {
     private LinkedList<T> cima;
     
